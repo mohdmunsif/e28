2fcd2adf173cad07ed89948615ea1097aa04fce9
Mohd Munsif <mohdmunsif@gmail.com>
2020-02-13 22:59:12 -0500

    fixing the JS to work on JSFiddle

--- game/e28week2.js
+++ game/e28week2.js
@@ -1,45 +1,48 @@
- 
 var options = ['rock', 'paper', 'scissors'];
-
-function runGame() {
-    // document.getElementById("demo").innerHTML = "Paragraph changed Haha.";
-
-    var userObj = new Object();
-    userObj.name = 'Player';
-    userObj.selection = "";
+let runGameBtn = document.querySelector('#runGame');
 
 
-    var compObj = new Object();
-    compObj.name = 'Computer';
-    compObj.selection = "";
+runGameBtn.addEventListener('click', runGame);
 
-    let userSel = document.querySelector('input[name="selection"]:checked');
+ function runGame() {
+   // document.getElementById("demo").innerHTML = "Paragraph changed Haha.";
 
-    userObj.selection = options.indexOf(userSel.value);
+   var userObj = new Object();
+   userObj.name = 'Player';
+   userObj.selection = "";
 
-    var min = 0;
-    var max = 3;
-    compObj.selection = Math.floor(Math.random() * (+max - +min)) + +min;
 
-    let span = document.getElementById("compSelection");
-    span.textContent = options[compObj.selection]; 
+   var compObj = new Object();
+   compObj.name = 'Computer';
+   compObj.selection = "";
 
-    document.getElementById("result").innerHTML = compare(compObj, userObj);
-}
- 
- 
-function compare(compObj, userObj) {
- 
-    if (compObj.selection == userObj.selection) {
-        return "Tie"
-    }
+   let userSel = document.querySelector('input[name="selection"]:checked');
 
-    if ( compObj.selection == userObj.selection + 1 || (userObj.selection == options.length && compObj.selection == 0) ) {
-        return compObj.name + " won with " + options[compObj.selection] + " vs " + userObj.name + "'s " + options[userObj.selection];
-    } else {
-        return userObj.name + " won with " + options[userObj.selection] + " vs " + compObj.name + "'s " +  options[compObj.selection];
-    }
-    
-    return " Random ";
-   
-}
+   userObj.selection = options.indexOf(userSel.value);
+
+   var min = 0;
+   var max = 3;
+   compObj.selection = Math.floor(Math.random() * (+max - +min)) + +min;
+
+   let span = document.getElementById("compSelection");
+   span.textContent = options[compObj.selection];
+
+   document.getElementById("result").innerHTML = compare(compObj, userObj);
+ }
+
+
+ function compare(compObj, userObj) {
+
+   if (compObj.selection == userObj.selection) {
+     return "Tie"
+   }
+
+   if (compObj.selection == userObj.selection + 1 || (userObj.selection == options.length && compObj.selection == 0)) {
+     return compObj.name + " won with " + options[compObj.selection] + " vs " + userObj.name + "'s " + options[userObj.selection];
+   } else {
+     return userObj.name + " won with " + options[userObj.selection] + " vs " + compObj.name + "'s " + options[compObj.selection];
+   }
+
+   return " Random ";
+
+ }
