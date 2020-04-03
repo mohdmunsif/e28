664034a9eae7ed145a2a8c69542c46bd489393c0
Munsif <mohdmunsif@gmail.com>
2020-04-02 23:50:48 -0400

    still WIP

--- gameVue/e28week3.js
+++ gameVue/e28week3.js
@@ -13,6 +13,51 @@ Vue.component('round-detail',{
   }
 })
 
+
+
+Vue.component('current-round',{
+  data: function() {
+    return {
+      id:'',
+      userChoice: '', 
+      choiceStack: ['paper', 'rock', 'scissors'],
+      userChoiceIndex: choiceStack.indexOf(userChoice),
+      compChoiceIndex: '',
+      compChoice: this.choiceStack[compChoiceIndex],
+      winner: '',
+      results: [
+        ['t', 'c', 'u'],
+        ['u', 't', 'c'],
+        ['c', 'u', 't'],
+        ],
+        shortRes: results[compChoiceIndex][userChoiceIndex],
+        resultMap: {
+          't': "Tie",
+          'u': "You win",
+          'c': "You lose"
+          },
+      result:'' 
+    }
+  },
+  template: '#current-round',
+  methods: {
+    compSelection: function () {
+      var min = 0;
+      var max = 3;
+      this.compChoiceIndex = Math.floor(Math.random() * (+max - +min)) + +min;
+    } ,
+    getWinner:function () {
+      result = resultMap[shortRes];
+      console.log('Winner is: ' + this.winner + ' User:' + this.userChoice + ', Comp:' + this.compChoice )
+    },
+    selectThis:function (userChoice) {
+      this.userChoice = userChoice;
+      this.compSelection();
+    }
+
+  }
+})
+
 Vue.component('round-selection',{
   data: function() {
     return {
@@ -32,64 +77,22 @@ Vue.component('round-selection',{
 var app = new Vue({
   el: '#app',
   data: {
-      'playerName': null,
+      playerName: "",
+      gameStart: false,
       wins: null,
       'picked': null,
-      roundsResult: ['Win', 'Lose', 'Win']
+      roundResult: {
+        id: '', 
+        userChoice:'',
+        compChoice:'',
+        winner:''},
+      roundResults: [ ]
   },
   methods: {
-    runGame2: function() {
-
+    startGame: function() {
+      this.gameStart = true;
     }
   }
 });
 
-
-var options = ['rock', 'paper', 'scissors'];
-let runGameBtn = document.querySelector('#runGame');
-
-
-runGameBtn.addEventListener('click', runGame);
-
- function runGame() {
-   // document.getElementById("demo").innerHTML = "Paragraph changed Haha.";
-
-   var userObj = new Object();
-   userObj.name = 'Player';
-   userObj.selection = "";
-
-
-   var compObj = new Object();
-   compObj.name = 'Computer';
-   compObj.selection = "";
-
-   let userSel = document.querySelector('input[name="selection"]:checked');
-
-   userObj.selection = options.indexOf(userSel.value);
-
-   var min = 0;
-   var max = 3;
-   compObj.selection = Math.floor(Math.random() * (+max - +min)) + +min;
-
-   let span = document.getElementById("compSelection");
-   span.textContent = options[compObj.selection];
-
-   document.getElementById("result").innerHTML = compare(compObj, userObj);
- }
-
-
- function compare(compObj, userObj) {
-
-   if (compObj.selection == userObj.selection) {
-     return "Tie"
-   }
-
-   if (compObj.selection == userObj.selection + 1 || (userObj.selection == options.length && compObj.selection == 0)) {
-     return compObj.name + " won with " + options[compObj.selection] + " vs " + userObj.name + "'s " + options[userObj.selection];
-   } else {
-     return userObj.name + " won with " + options[userObj.selection] + " vs " + compObj.name + "'s " + options[compObj.selection];
-   }
-
-   return " Random ";
-
- }
+ 
