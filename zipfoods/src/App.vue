0afde2463246a59bbb4d2bf1f78a7dc706fadeee
munsif <mohdmunsif@gmail.com>
2020-05-03 09:38:23 -0400

    initial before Vue router

--- zipfoods/src/App.vue
+++ zipfoods/src/App.vue
@@ -1,28 +1,39 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/zipfoods-logo.png" id="logo">
-    
-    <p>
-      ZipFoods is your one-stop-shop for convenient online grocery shopping in the greater Boston area.
-      </p>
 
+
+      <show-home></show-home>
       <show-featured :category='"snacks"'></show-featured>
       <show-products></show-products>
+      <show-categories></show-categories>
+
+      <router-view></router-view>
 
   </div>
 </template>
 
 <script>
 
-import ShowProducts from './components/ShowProducts.vue';
-import ShowFeatured from './components/ShowFeatured.vue';
+import { products } from './products.js'; 
+import ShowProducts from './components/ShowProducts.vue'; 
+import ShowCategories from './components/ShowCategories.vue';
+import ShowHome from './components/ShowHome.vue';
+
+
  
 export default {
   name: 'App',
   components: {
     'show-products': ShowProducts,
-    'show-featured': ShowFeatured,
-     
+    'show-categories': ShowCategories,
+    'show-home': ShowHome,    
+ 
+  },
+  data: function() {
+    return {
+      products: products
+    };
   }
 }
 </script>
